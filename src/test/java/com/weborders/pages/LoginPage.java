b0918dcbdd625e45931a5a70c591a91d7fffc320
bulentselcuk <bulentselcuk@gmail.com>
2020-07-16 09:08:24 -0500

    “firstcommit“

---
+++ src/test/java/com/weborders/pages/LoginPage.java
@@ -0,0 +1,23 @@
+package com.weborders.pages;
+
+import com.weborders.utilities.BrowserUtilities;
+import com.weborders.utilities.ConfigurationReader;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage{
+
+    @FindBy(id="ctl00_MainContent_username")
+    private WebElement userName;
+    @FindBy(id="ctl00_MainContent_password")
+    private WebElement password;
+
+    public void login() { // Orginal way...
+        BrowserUtilities.wait(2);
+        String usernameValue = ConfigurationReader.getProperty("username");
+        String passwordValue = ConfigurationReader.getProperty("password");
+        userName.sendKeys(usernameValue);
+        password.sendKeys(passwordValue, Keys.ENTER);
+    }
+    }
